58caca08d104c76f900c110531de7fd5c55dbef0
kittipongkp <kittipong.mesut@gmail.com>
2018-06-24 13:44:20 +0700

    test hello

--- src/index.js
+++ src/index.js
@@ -3,8 +3,8 @@ import path from 'path';
 
 const app = express();
 
-app.get('/*',(req, res)=>{
-    res.sendFile(path.join(__dirname, index.html));
-})
+app.get("/*", (req, res) => {
+    res.sendFile(path.join(__dirname, "index.html"));
+  });
 
 app.listen(8080, ()=>console.log("Running on localhost:8080"));
